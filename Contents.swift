9bdc721749ec0d64a0fc6a5465dadd1d543afa26
vicurr1 <victorpanderetas@hotmail.com>
2015-11-19 03:31:09 +0100

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -6,12 +6,12 @@ for i in 0..<101 {
     if i%2 == 0 {
         print("\(i) par!!!")
     }
-    if i%2 != 0 {
+    else if i%2 != 0 {
         print("\(i) impar!!!")
-        if i%5==0 {
-            print("\(i) Bingo!!!")
-        }
     }
+    if i%5==0 {
+        print("\(i) Bingo!!!")
+    }    
     if i>=30 && i<=40 {
         print("\(i) Viva Swift!!!")
     }
